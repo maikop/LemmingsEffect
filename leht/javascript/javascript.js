c85f7beadb51b3dc14dbfa4a4689c301a4888bce
lihur <pihlam2gi@gmail.com>
2014-03-13 01:19:48 +0200

    Liikuv header koos (vajadusel ilmuva) logoga.

--- leht/javascript/javascript.js
+++ leht/javascript/javascript.js
@@ -1,18 +1,30 @@
-//<![CDATA[ 
-function movescroll(korgus){
-	if ($(window).scrollTop() >= korgus)
-    {
-        $("#HeaderMenu").css({position:'fixed',left:'0px',top:'0px'});
+//<![CDATA[
+var nameSpace = {
+	lukus: false,
+	korgus : 136
+};
+function movescroll(){
+	var korgus = nameSpace.korgus;
+	if ($(window).scrollTop() >= korgus){
+		$("#HeaderMenu").css({position:'fixed',left:'0px',top:'0px'});
+		if (!nameSpace.lukus){
+			nameSpace.lukus=true;
+			$("#HeaderCenterMenu").animate({left:'80px'},100,function(){nameSpace.lukus=false;});
+		}
     }
     else
     {
         $("#HeaderMenu").css({position:'absolute',left:'0px',top:korgus+'px'});
+	if (!nameSpace.lukus){
+	      nameSpace.lukus=true;
+	      $("#HeaderCenterMenu").animate({left:'0px'},100,function(){nameSpace.lukus=false;});
+	}
     }
 }
 $(window).load(function(){
-	var korgus=170;
-	movescroll(korgus);
+	nameSpace.korgus=(parseInt($('#HeaderMenu').css('top')));
+	movescroll();
 $(window).scroll(function(){
-    movescroll(korgus);
+	movescroll();
 });
 });//]]>  
