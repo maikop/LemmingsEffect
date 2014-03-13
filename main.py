4753a9b96a04b1f8b92d98853fcfe306771b602f
roffelkopter <roffelkopter@hotmail.com>
2014-03-13 19:35:07 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# Copyright 2012 Digital Inspiration
+# http://www.labnol.org/
+
+import os
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp import template
+import sqlite3
+
+class MainPage(webapp.RequestHandler):
+  def get (self, q):
+    if q is None:
+      q = 'index.html'
+
+    path = os.path.join (os.path.dirname (__file__), q)
+    self.response.headers ['Content-Type'] = 'text/html'
+    self.response.out.write (template.render (path, {}))
+
+app = webapp.WSGIApplication([('/(.*html)?', MainPage)], debug = True)
+util.run_wsgi_app (app)
