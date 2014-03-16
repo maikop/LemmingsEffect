c628b21ad69f3ebf423c02a22be545a7321f2553
lihur <pihlam2gi@gmail.com>
2014-03-16 03:02:46 +0200

    Javascript loetavam, konqueror, rekonq ja vanad chromed ei tee vigu logoga, väikeste ekraanide muudatus

--- leht/javascript/javascript.js
+++ leht/javascript/javascript.js
@@ -1,12 +1,14 @@
 //<![CDATA[
 //Muutujad
 var nameSpace = {
-    lukus: false,
-    korgus: 150,
-    korda:1000,
-    animatsioon:200,
-    fixedheight:200,
-    width: 600,
+    lukus: false, //Animatsiooni lukustus (muidu saadaks see pidevalt scrollides animatsioone ja lõhuks lehe)
+    korgus: 150, //Headeri Menüü kõrgus (uurib automaatselt korrektse järgi)
+    korda: 1000, //Ajavahemik, mille jooksul aktiveeritakse movescroll
+    animatsioon: 200, //Animatsiooni kiirus
+    fixedheight: 200, //Jätab menüü jaoks maksimaalselt nii palju vaba ruumi (enamvähem headeri kõrgus)
+    minfixedheight: 50, //Menüü jätab pisut vaba ruumi
+    width: 600, //Ekraanilaius, mille korral aktiveerib menüünupu
+    minwidth: 354 //Ekraanilaius, millest väiksema puhul ei too logo nähtavale
 };
 //Kui akna asukoht madalam kui kindel kõrgus, too logo nähtavale ja fikseeri header
 //Kui akna asukoht kõrgem, peida logo ja lase header lahti
@@ -17,10 +19,17 @@ function movescroll() {
             left: '0px',
             top: '0px'
         });
-        if (!nameSpace.lukus) {
+        if (!nameSpace.lukus && $(window).width() > nameSpace.minwidth) {
             nameSpace.lukus = true;
             $("#HeaderCenterMenu").animate({
                 left: '90px'
+            }, nameSpace.animatsioon, function () {
+                nameSpace.lukus = false;
+            });
+        } else if (!nameSpace.lukus && $(window).width() <= nameSpace.minwidth) { //Kui ekraan väiksem, peidab logo
+            nameSpace.lukus = true;
+            $("#HeaderCenterMenu").animate({
+                left: '0px'
             }, nameSpace.animatsioon, function () {
                 nameSpace.lukus = false;
             });
@@ -29,7 +38,7 @@ function movescroll() {
         $("#HeaderMenu").css({
             position: 'relative',
             left: '0px',
-            top: nameSpace.korgus +"px"
+            top: nameSpace.korgus + "px"
         });
         if (!nameSpace.lukus) {
             nameSpace.lukus = true;
@@ -41,34 +50,45 @@ function movescroll() {
         }
     }
 }
-$(function() {
-	  var menubutton	= $('#headerMenuButton');
-		menu		= $('#HeaderCenterMenu ul');
-		menuHeight  = menu.height();
-		var $body = $("body");
-		//Paneb esimest korda menüü korguse paika
-		if($(window).width() < nameSpace.width && menu.is(':hidden')) {
-			$('#HeaderCenterMenu ul').css({height: $body.height()-nameSpace.fixedheight})
-		}
-		$(menubutton).on('click', function(e) {
-			e.preventDefault();  
-			menu.slideToggle();
-		});
+$(function () {
+    var menubutton = $('#headerMenuButton');
+    menu = $('#HeaderCenterMenu ul');
+    menuHeight = menu.height();
+    var $body = $("body");
+    var korgus = Math.max(nameSpace.fixedheight - $(window).scrollTop(), nameSpace.minfixedheight);
+    //Paneb esimest korda menüü korguse paika
+    if ($(window).width() < nameSpace.width && menu.is(':hidden')) {
+        $('#HeaderCenterMenu ul').css({
+            height: $("body").height() - korgus
+        });
+    }
+    $(menubutton).on('click', function (e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+    $(window).scroll(function () {
+        korgus = Math.max(nameSpace.fixedheight - $(window).scrollTop(), nameSpace.minfixedheight);
+        $('#HeaderCenterMenu ul').css({
+            height: $("body").height() - korgus
+        });
+    });
 
-		$(window).resize(function(){  
-			var w = $(window).width();
-			var $body = $("body");
-			var fixedHeight = $("#HeaderCenterMenu ul").height();
-			if(w > nameSpace.width && menu.is(':hidden')) {  
-				menu.removeAttr('style');  
-			}
-			$('#HeaderCenterMenu ul').css({height: $body.height()-nameSpace.fixedheight});
-		});
+    $(window).resize(function () {
+        var w = $(window).width();
+        var $body = $("body");
+        var fixedHeight = $("#HeaderCenterMenu ul").height();
+        if (w > nameSpace.width && menu.is(':hidden')) {
+            menu.removeAttr('style');
+        }
+        $('#HeaderCenterMenu ul').css({
+            height: $body.height() - korgus
+        });
+    });
 });
 $(window).load(function () {
-    
+
     //Kui leht laetaks, uurib css-ist kõrguse järgi
-    nameSpace.korgus = (parseInt($('#HeaderMenu').css('top')));
+    nameSpace.korgus = (parseInt($('#HeaderMenu').css('top'), 10));
     movescroll();
     $(window).scroll(function () {
         movescroll();
