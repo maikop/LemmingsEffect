8afcf8f433fc7a21d5d38048521fcc5e80936f11
roffelkopter <roffelkopter@hotmail.com>
2014-03-24 05:06:15 +0200

    Tegin nats uuemaks ja ilusamaks. Lisasin algse crawleri.

--- uudisreader/reader/models.py
+++ uudisreader/reader/models.py
@@ -2,20 +2,18 @@ from django.db import models
 import feedparser
 
 class Lehed(models.Model):
-    Name = models.CharField(max_length=200)
-    Address = models.CharField(max_length=200)
-    Adddate = models.DateTimeField('date published')
+    name = models.CharField(max_length=2000)
+    address = models.CharField(max_length=2000)
     
     def __str__(self):
-        return self.Name
+        return self.name
         
 class Uudised(models.Model):
-    Title = models.CharField(max_length=200)
-    Description = models.CharField(max_length=200)
-    Link = models.CharField(max_length=200)
-    Published = models.CharField(max_length=200)
-    Adddate = models.DateTimeField('date published')
+    title = models.CharField(max_length=2000)
+    description = models.CharField(max_length=2000)
+    link = models.CharField(max_length=2000)
+    published = models.CharField(max_length=2000)
     
     def __str__(self):
-        return self.Title
+        return self.title
         
